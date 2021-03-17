a20c8676d77e89a862ea159ed5ae2247234fa6e5
Ian Hancock <northroadian@gmail.com>
2021-03-17 15:57:28 -0500

    added more arrays

--- src/App.js
+++ src/App.js
@@ -1,38 +1,35 @@
+// 
 import React, { Component } from 'react'
-const list =[1,2,3]
-export default class App extends Component {
-  
-  constructor(props){
-    super(props)
-    this.state ={
-      list,
-    }
-  }
 
-  onClearArray =()=>{
-    this.setState({list:[]});
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state={
+      value: '',
+      list: [1,2,3,4,5]
+    }
+     
   }
-  onResetArray =()=>{
-    this.setState({list})
+  onAddItem = () =>{
+    this.setState(state=>{
+      const list = state.list.concat(this.state.value)
+
+      return {
+        list, value:''
+      };
+    });
+  };
+ 
   }
   render() {
     return (
       <div>
-        <ul>
-          {this.state.list.map(item =>(
-            <li key={item}>{item}</li>
-          ))}
-        </ul>
-        <button type='button' onClick={this.onClearArray}>
-          Clear Away
-        </button>
-        
-        <button type="button" onClick={this.onResetArray}> Reset</button>
         
       </div>
     )
   }
 }
+
 
 
 //As mentioned, the initial array state is done in the React component's constructor and afterward used within the render() method to display it. Every time the state changes, the render method will run again and display the correct state in your browser. However, the state shouldn't be changed by mutating this.state directly. Instead, there exists the this.setState() method on a React component to update React's state. A quote from the official React documentation says:
